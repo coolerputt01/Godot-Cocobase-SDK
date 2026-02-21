e23e4dfbcd46cf57596c5a1d78f31c222ad48c04
coolerputt01 <olumideadekolu9@gmail>
2026-02-21 00:50:15 -0800

    Made some new updates

--- addons/request_helper.gd
+++ addons/request_helper.gd
@@ -3,14 +3,17 @@ extends Node
 class_name Cocobase
 
 var apiUrl := "https://api.cocobase.buzz";
-var ws_url = ""
+var ws_url = "";
+const BASE_WS_URL := "wss://cloud.cocobase.buzz"
 var apiKey := "";
+var projectId = "";
 
 var ws := WebSocketClient.new()
 
 var reconnect_delay = 1.0
 var max_reconnect_delay = 30.0
 var should_reconnect = true
+var connection_mode = ""
 
 signal connected
 signal message_received(data)
@@ -18,14 +21,45 @@ signal closed
 signal document_created(data)
 signal document_updated(data)
 signal document_deleted(data)
+signal error(data)
+signal room_created(data)
+signal user_joined(data)
+signal user_left(data)
+
 
 var auth_data = {}
 var current_collection = ""
 var filters = {}
 
+var ping_timer : Timer
 
-func init(_apikey):
+
+func init(_apikey,_projectId):
 	apiKey = _apikey;
+	projectId = _projectId;
+
+func load_env(path:String="res://.env"):
+	var env = {}
+	var file = File.new()
+
+	if not file.file_exists(path):
+		print("No .env file found")
+		return env
+
+	file.open(path, File.READ)
+	while not file.eof_reached():
+		var line = file.get_line().strip_edges()
+		if line == "" or line.begins_with("#"):
+			continue
+
+		var parts = line.split("=")
+
+		if parts.size() >= 2:
+			var key = parts[0].strip_edges()
+			var value = parts[1].strip_edges()
+			env[key] = value
+	file.close()
+	return env
 
 func send_request(path: String, method := HTTPClient.METHOD_GET, body : Dictionary = {}):
 	var url = apiUrl + path;
@@ -50,10 +84,19 @@ func send_request(path: String, method := HTTPClient.METHOD_GET, body : Dictiona
 
 func _on_request_completed(result, response_code, headers, body,request_node):
 	var text = body.get_string_from_utf8()
-	var data = {}
-	if text != "":
-		data = JSON.parse(text)
-	print(data.result)
+
+	if text == "":
+		request_node.queue_free()
+		return
+
+	var parse = JSON.parse(text)
+	if parse.error != OK:
+		emit_signal("error", {"error": "Invalid JSON response"})
+		request_node.queue_free()
+		return
+
+	var data = parse.result
+	print(data)
 	request_node.queue_free();
 
 func create_collection(collectionName: String):
@@ -149,11 +192,26 @@ func resendVerificationEmail():
 
 
 func _process(delta):
-	if ws and ws.get_connection_status() != WebSocketClient.CONNECTION_DISCONNECTED:
+	if ws and (
+		ws.get_connection_status() == WebSocketClient.CONNECTION_CONNECTING or
+		ws.get_connection_status() == WebSocketClient.CONNECTION_CONNECTED
+	):
 		ws.poll()
 
 
+
 func _start_websocket():
+
+	if ping_timer:
+		ping_timer.stop()
+		ping_timer.queue_free()
+		ping_timer = null
+
+	if ws:
+		ws.disconnect_from_host()
+		ws = null
+
+
 	# Always recreate socket
 	ws = WebSocketClient.new()
 
@@ -162,37 +220,60 @@ func _start_websocket():
 	ws.connect("connection_error", self, "_on_closed")
 	ws.connect("data_received", self, "_on_data")
 
+	ping_timer = Timer.new()
+	ping_timer.wait_time = 30
+	ping_timer.autostart = true
+	ping_timer.one_shot = false
+	add_child(ping_timer)
+	ping_timer.connect("timeout", self, "_send_ping")
+
+
 	var err = ws.connect_to_url(ws_url)
 	if err != OK:
-		print("Watcher connection error")
+		emit_signal("error", {"error": "Connection failed"})
 
 	set_process(true)
 
 
 func _on_connected(protocol):
 	reconnect_delay = 1.0
-	send(auth_data)
+	if auth_data != {}:
+		send(auth_data)
 	emit_signal("connected")
 
-func _on_closed(was_clean = false):
+func _on_closed():
+	print("⚡Connection closed!")
 	emit_signal("closed")
 
-	if should_reconnect and current_collection != "":
+	if should_reconnect and connection_mode != "":
 		yield(get_tree().create_timer(reconnect_delay), "timeout")
 		reconnect_delay = min(reconnect_delay * 2, max_reconnect_delay)
 		_start_websocket()
 
 func _on_data():
+	if not ws:
+		return
+
 	var peer = ws.get_peer(1)
+	if not peer:
+		return
+
+	if peer.get_available_packet_count() == 0:
+		return
+
 	var pkt = peer.get_packet().get_string_from_utf8()
+	print("Received raw data:", pkt)
 
 	var parsed = JSON.parse(pkt)
 	if parsed.error != OK:
-		print("Invalid JSON")
+		emit_signal("error", {"error": "Invalid JSON", "data": pkt})
 		return
+
 
 	var data = parsed.result
 	var event_type = data.get("event", "")
+
+	emit_signal("message_received", data)
 
 	match event_type:
 		"connected":
@@ -203,26 +284,105 @@ func _on_data():
 			emit_signal("document_updated", data)
 		"delete":
 			emit_signal("document_deleted", data)
+		"room_created":
+			emit_signal("room_created", data)
+		"user_joined":
+			emit_signal("user_joined", data)
+		"user_left":
+			emit_signal("user_left", data)
 		_:
 			print("Unknown event:", event_type)
 
 
+
 func send(data):
-		if ws.get_connection_status() == WebSocketClient.CONNECTION_CONNECTED:
+		if ws and  ws.get_connection_status() == WebSocketClient.CONNECTION_CONNECTED:
 			var json = JSON.print(data)
 			ws.get_peer(1).put_packet(json.to_utf8())
+
+func _send_ping():
+	send({"type": "ping"})
+
+func send_room_message(content):
+	send({
+		"type": "message",
+		"content": content
+	})
+
+# wss://cloud.cocobase.buzz/ws/3dc34222-9472-4cce-8712-125efa250bd8/websockets
+
+func connect_broadcast(func_name):
+	should_reconnect = true
+	current_collection = ""
+	connection_mode = "broadcast"
+
+	#ws_url = "wss://cloud.cocobase.buzz/ws/"+projectId+"/"+func_name
+	ws_url = "wss://api.cocobase.buzz/realtime/broadcast";
+	auth_data = {
+		"api_key": apiKey
+	}
+	_start_websocket()
 
 
 func close():
 	should_reconnect = false
-	ws.disconnect_from_host()
+
+	if ping_timer:
+		ping_timer.stop()
+		ping_timer.queue_free()
+		ping_timer = null
+
+	if ws:
+		ws.disconnect_from_host()
+
+	set_process(false)
+
+func create_room(room_id:String, room_title:String = ""):
+	should_reconnect = true
+	current_collection = ""
+	connection_mode = "room"
+
+	ws_url = "wss://api.cocobase.buzz/realtime/rooms/" + room_id
+
+	auth_data = {
+		"api_key": apiKey,
+		"action": "create",
+		"room_title": room_title,
+		"user_id": "0",
+		"user_name": "gamer002"
+	}
+
+	_start_websocket()
+
+
+func join_room(room_id:String,room_title: String):
+	should_reconnect = true
+	current_collection = ""
+	connection_mode = "room"
+
+	print("hi")
+
+	ws_url = "wss://api.cocobase.buzz/realtime/rooms/" + room_id
+
+	auth_data = {
+		"api_key": apiKey,
+		"action": "join",
+		"room_title": room_title,
+		"user_id": "0",
+		"user_name": "gamer002"
+	}
+	_start_websocket()
+
+
 
 func watch_collection(collection_name:String, _filters:Dictionary = {}):
+
+	should_reconnect = true
+	connection_mode = "collection"
 	current_collection = collection_name
 	filters = _filters
 
-	ws_url = apiUrl.replace("https://", "wss://").replace("http://", "ws://")
-	ws_url += "/realtime/collections/" + collection_name
+	ws_url = BASE_WS_URL + "/realtime/collections/" + collection_name
 
 	auth_data = {
 		"api_key": apiKey,
--- tests/tests.gd
+++ tests/tests.gd
@@ -3,21 +3,50 @@ extends Node2D
 
 var cocobase = Cocobase.new();
 func _ready() -> void:
+	var env = cocobase.load_env();
+	print(env);
 	add_child(cocobase);
-	cocobase.init("npYdLh6y1363mC8hLiTlf94VL1dDO13TTjwdTL6N");
-	cocobase.get_all_documents("989641d9-61b9-4bc5-a317-b0e347bb347b");
+	cocobase.init(env["COCOBASE_KEY"],env["COCOBASE_PROJECT_ID"]);
 
-
+	# Cocobase Basic signals.
 	cocobase.connect("connected", self, "_on_connected")
-	cocobase.connect("document_created", self, "_on_created")
-	cocobase.connect("document_updated", self, "_on_updated")
-	cocobase.connect("document_deleted", self, "_on_deleted")
+	cocobase.connect("message_received", self, "_on_message")
+	cocobase.connect("error", self, "_on_error")
 	cocobase.connect("closed", self, "_on_closed")
+	cocobase.connect("room_created", self, "_on_room_created")
+	cocobase.connect("user_joined", self, "_on_user_joined")
+	cocobase.connect("user_left", self, "_on_user_left")
 
-	cocobase.watch_collection("tasks")
+	cocobase.create_room("room1","Lobby-1")
+	#cocobase._process()
 
-	cocobase.create_document("tasks", {
-		"title": "Realtime test",
-		"done": false
-	})
 
+# Signal callbacks.
+func _on_connected(data = {}):
+	print("‚úÖConnected to WebSocket!", data)
+
+func _on_message(data):
+	print("Ì†ΩÌ≥®Message received:", data)
+
+func _on_error(data):
+	print("‚ùåWebSocket error:", data)
+
+func _on_closed():
+	print("‚ö°Connection closed!")
+
+func _on_room_created(data):
+	print("Ì†ºÌæâRoom created:", data)
+	var auth_data = {
+		"api_key": cocobase.apiKey,
+		"action": "join",
+		"room_title": "Lobby-1",
+		"user_id": "0",
+		"user_name": "gamer002"
+	}
+	cocobase.send(auth_data)
+
+func _on_user_joined(data):
+	print("Ì†ΩÌ±§User joined:", data)
+
+func _on_user_left(data):
+	print("Ì†ΩÌ±§User left:", data)
